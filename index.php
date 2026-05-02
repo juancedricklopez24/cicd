0b3c6b398462378ac5efc6b7c64d4f9964a2f02f
juancedricklopez24 <juancedricklopez@gmail.com>
2026-05-02 08:56:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sample Webpage</title>
+</head>
+<body>
 
-echo "uwuuuuuuuu";
+    <h1>Welcome to My Website</h1>
+    <p>This is a simple paragraph of text in HTML.</p>
+    
+    <a href="https://example.com">This is a link to another site</a>
 
+    <h3>Things to do today:</h3>
+    <ul>
+        <li>Learn basic HTML tags</li>
+        <li>Create a sample file</li>
+        <li>View it in a browser</li>
+    </ul>
 
-?>
+</body>
+</html>
